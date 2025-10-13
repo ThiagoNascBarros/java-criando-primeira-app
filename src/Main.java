02da666fb5ab32449e4da9fa57cfa0e0a433b510
ThiagoNascBarros <thiago171nasciment@gmail.com>
2025-10-13 11:18:56 -0300

    feat: Adicionando Casting para conversão de tipos no Java (Double -> Int)

--- src/Main.java
+++ src/Main.java
@@ -24,5 +24,11 @@ public class Main {
                 Ano de lançamento:
                 """ + anoDeLancamento;
         System.out.println(sinopse);
+
+        int classificacao;
+        // Casting -  Transformando valores no Java - Double -> Int
+        classificacao = (int) media / 2;
+        System.out.println(classificacao);
+
     }
 }
